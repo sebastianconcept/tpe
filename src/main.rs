1fd04a861952906d8d631f8a41ffdc9811362ea1
Sebastian Sastre <sebastianconcept@gmail.com>
2024-03-19 07:24:16 -0300

    adds case1.csv and the InvalidInput enum

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,42 @@
+use std::{env, error, fmt, fs};
 use std::error::Error;
 
-fn digest_input() -> Result<u8, Box<dyn Error>> {
-    Ok(65u8)
+#[derive(Debug)]
+enum InvalidInput {
+    MissingInputFilename,
+    FileNotFound(String),
 }
 
+impl fmt::Display for InvalidInput {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "Invalid input")
+    }
+}
 
-fn main() -> Result<(), Box<dyn Error>>{
-    let result = digest_input()?;
-    println!("{}",result);
+impl error::Error for InvalidInput {}
+
+// impl error::Error for InvalidInput {}
+
+fn get_input_filename() -> Option<String> {
+    env::args().nth(1)
+}
+
+fn digest_input() -> Result<String, InvalidInput> {
+    match get_input_filename() {
+        None => Err(InvalidInput::MissingInputFilename),
+        Some(filename) => {
+            let file_exists = fs::metadata(&filename).is_ok();
+            if !file_exists {
+                return Err(InvalidInput::FileNotFound(filename));
+            }
+            Ok("Input digested".to_string())
+        }
+    }
+}
+
+fn main() -> Result<(), Box<dyn Error>> {
+    // process_input()?;
+    digest_input()?;
+    println!("Done well");
     Ok(())
 }
